4437e70ecc7d916eb3cff91bb943817a8359f6be
vivek <vivekcloud123@gmail.com>
2025-09-06 14:46:32 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
